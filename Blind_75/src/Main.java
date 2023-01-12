66720be0ed5fdff2c2098903a6ae4d832ca4f7e0
Widmaer09 <widmaer09@gmail.com>
2023-01-11 22:41:35 -0500

    2nd commit

--- Blind_75/src/Main.java
+++ Blind_75/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> allValues = new HashSet<>();
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (allValues.contains(nums[i])) {
+
+                return true;
+            } else {
+                allValues.add(nums[i]);
+            }
+        }
+        return false;
+
+
     }
+
+    public static void main(String[] args) {
+        int Arraytest[] = {1, 2, 3, 1};
+
+
+        System.out.println(containsDuplicate(Arraytest));
+
+
+    }
+
+
 }
